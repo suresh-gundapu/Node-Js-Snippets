41aa1c31eb220a26518f83f56ccfb2c136b6478a
suresh-gundapu <suresh.gundapu2021@gmail.com>
2024-03-02 09:52:19 +0530

    New updates

---
+++ FS Module/module_ex1.js
@@ -0,0 +1,14 @@
+let a = 2;
+
+let name = "suresh";
+
+let func1 = function(){
+
+  console.log("hi");
+}
+
+exports.number = a;
+
+exports.name1 = name;
+
+exports.function_name = func1;
---
+++ FS Module/module_ex2.js
@@ -0,0 +1,5 @@
+var refVar = require('./module_ex1.js');
+
+console.log(refVar.name1);
+console.log(refVar.number);
+console.log(refVar.function_name);
